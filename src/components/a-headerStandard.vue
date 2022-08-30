01b2c0f73a8648df875f885d475e5fedf9c46821
Your Name <you@example.com>
2022-08-30 12:32:57 +0200

    search

--- src/components/a-headerStandard.vue
+++ src/components/a-headerStandard.vue
@@ -55,8 +55,46 @@
       <span class="links" v-for="(name, i) in menu" :key="i">{{ name }}</span>
     </div>
     <div class="searchbar">
-      <input class="inp w-50" type="search" />
-      <i class="fas fa-search ic"></i>
+      <input class="inp d-none d-md-block w-50" type="search" />
+      <i
+        data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop"
+        class="fas fa-search ic"
+      ></i>
+      <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
